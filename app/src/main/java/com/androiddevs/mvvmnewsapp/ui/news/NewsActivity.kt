718c61e8ae765a03117adc8b2bcf8f59dd594519
sarafa <saraf@arabia-it.com>
2020-12-23 16:07:37 +0200

    add view binding gradle

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/news/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/news/NewsActivity.kt
@@ -1,35 +1,35 @@
-package com.androiddevs.mvvmnewsapp.ui.news
+package com.androiddevs.mvvmnewsapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.mvvmnewsapp.R
-import com.androiddevs.mvvmnewsapp.data.local.datasource.NewsLocalDataSource
-import com.androiddevs.mvvmnewsapp.data.local.db.ArticleDatabase
-import com.androiddevs.mvvmnewsapp.data.remote.api.RetrofitInstance
-import com.androiddevs.mvvmnewsapp.data.remote.datasource.NewsRemoteDataSource
-import com.androiddevs.mvvmnewsapp.data.repository.NewsRepository
-import com.androiddevs.mvvmnewsapp.util.NetworkHelper
+import com.androiddevs.mvvmnewsapp.databinding.ActivityNewsBinding
+
+import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
 import kotlinx.android.synthetic.main.activity_news.*
+
 
 class NewsActivity : AppCompatActivity() {
 
     lateinit var viewModel: NewsViewModel
-
+    lateinit var binding: ActivityNewsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_news)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val newsRepository =
-            NewsRepository(
-                NewsLocalDataSource(ArticleDatabase(this).getArticleDao()),
-                NewsRemoteDataSource(RetrofitInstance.api),
-                NetworkHelper(this)
-            )
+        val newsRepository = NewsRepository(ArticleDatabase(this))
         val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
         viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
-        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+        bottomNavigationView.setupWithNavController(
+            findNavController(
+                this,
+                R.id.newsNavHostFragment
+            )
+        )
     }
 }
