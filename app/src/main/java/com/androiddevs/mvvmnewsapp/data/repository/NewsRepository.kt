f2a4f7c2d74491eaa1f2fe423d775552a735d454
sarafa <saraf@arabia-it.com>
2021-01-02 22:17:52 +0200

    rebase branch from master

--- app/src/main/java/com/androiddevs/mvvmnewsapp/data/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/data/repository/NewsRepository.kt
@@ -9,9 +9,7 @@ import com.androiddevs.mvvmnewsapp.domain.models.Article
 import com.androiddevs.mvvmnewsapp.domain.models.NewsResponse
 import com.androiddevs.mvvmnewsapp.domain.repository.NewsRepository
 import com.androiddevs.mvvmnewsapp.util.BaseNetworkHelper
-import com.androiddevs.mvvmnewsapp.util.NetworkHelper
 import dagger.hilt.android.scopes.ActivityRetainedScoped
-import java.io.IOException
 import javax.inject.Inject
 
 @ActivityRetainedScoped
@@ -28,51 +26,12 @@ class NewsRepository @Inject constructor(
         return SafeApiCall(networkHelper) {
             newsRemoteDataSource.getBreakingNews(countryCode, pageNumber)
         }
-        /*try {
-            if (networkHelper.hasInternetConnection()) {
-                val response = newsRemoteDataSource.getBreakingNews(countryCode, pageNumber)
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        return Resource.Success(it)
-                    }
-                }
-                return Resource.Error(response.message())
-            } else {
-                return Resource.Error("No internet connection")
-            }
-        } catch (t: Throwable) {
-            when (t) {
-                is IOException -> return Resource.Error("Network Failure")
-                else -> return Resource.Error("Conversion Error")
-            }
-        }*/
     }
 
     override suspend fun searchNews(searchQuery: String, pageNumber: Int): Resource<NewsResponse> {
         return SafeApiCall(networkHelper) {
             newsRemoteDataSource.searchForNews(searchQuery, pageNumber)
         }
-        /*  try {
-              if (networkHelper.hasInternetConnection()) {
-                  val response = newsRemoteDataSource.searchForNews(searchQuery, pageNumber)
-                  if (response.isSuccessful) {
-                      response.body()?.let {
-                          return Resource.Success(it)
-                      }
-                  }
-                  return Resource.Error(response.message())
-
-
-              } else {
-                  return Resource.Error("No internet connection")
-              }
-          } catch (t: Throwable) {
-              when (t) {
-                  is IOException -> return Resource.Error("Network Failure")
-                  else -> return Resource.Error("Conversion Error")
-              }
-          }*/
-
     }
 
     override suspend fun upsert(article: Article) =
